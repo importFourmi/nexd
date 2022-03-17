9d03854fcb4784d81d698c73738b2cae8e1be395
importFourmi <94900332+importFourmi@users.noreply.github.com>
2022-03-17 01:59:44 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+     name='nexd',  
+     version='0.1',
+     scripts=['nexd'] ,
+     author="importFourmi",
+     author_email="calvin.galagain@gmail.com",
+     description="Python utility package",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/importFourmi/nexd",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+     ],
+ )
